47e0cff1abe030f32aaf6139351fb34ffb0d1824
superherolegendsheep <114862943+superherolegendsheep@users.noreply.github.com>
2026-09-03 15:21:57 +0200

    四次软装

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   publicDir: "public",
   build: {
     outDir: "dist",
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin.html")
+      }
+    }
   }
 });
